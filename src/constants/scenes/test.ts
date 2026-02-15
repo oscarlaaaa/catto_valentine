aedcb478871d2014856aadb903ee2b79bfa27029
Oscar La <oscarla5747@gmail.com>
2026-02-14 23:38:38 -0500

    zzz

--- src/constants/scenes/test.ts
+++ src/constants/scenes/test.ts
@@ -1,7 +1,2 @@
-import { DOOR_SCENE } from "./1_door_scene";
-import { PARK_SCENE } from "./2_park_scene";
-import { RESTAURANT_SCENE, RESTAURANT_START } from "./3_restaurant_scene";
-import { END_CREDIT_SCENE, SLIDESHOW_SCREEN_ID } from "./4_credit_scene";
-
 export const INITIAL_SCENE_TESTING = null;
 export const INITIAL_CONTENT_TESTING = null;
