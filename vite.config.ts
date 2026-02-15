8b79a2494f19594bef9e0e0534f190adffbb310f
Oscar La <oscarla5747@gmail.com>
2026-02-14 23:37:42 -0500

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
   server: {
     watch: {
       usePolling: true,
